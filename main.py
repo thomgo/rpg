cd52d10eca4b3c89b21efb3cfda0222f0bcc8b1d
Thomas <tg.59290@gmail.com>
2019-08-26 11:36:07 +0200

    add comments in main.py

--- main.py
+++ main.py
@@ -5,15 +5,20 @@ from game.factory import Factory
 from game.arena import Arena
 
 if __name__ == '__main__':
+    # Start a narrator to tell the story
     narrator = Narrator()
+    # Start an arena where battles are going to take place
     arena = Arena()
 
     narrator.introduction()
+    # Allow the user to choose his type of character and retrieve it from the factory
     choice = narrator.choose_character()
     player = Factory.get_character(choice)
     narrator.player_customization(player)
+    # Retrieve an ennemy from factory, for this scenario an orc
     ennemy = Factory.get_ennemy('orc')
 
+    # Exemple story to be told by the narrator
     story = [
         'Votre voyage commence. Vous marchez au bord des imposantes montagne de la Moria',
         'Le soleil est chaud sur votre visage et la neige fraîche à vos pieds',
@@ -22,5 +27,6 @@ if __name__ == '__main__':
     ]
     narrator.tell(story)
 
+    # Start a fight
     arena.fighters_enter(player, ennemy)
     arena.battle()
